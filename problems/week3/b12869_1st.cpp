d19858cff82bb1f09110003a7df6fcceb642ffbb
Rachel Lee <engrrachel318@gmail.com>
2023-11-02 21:11:24 +0900

    feat: add problems

---
+++ problems/week3/b12869_1st.cpp
@@ -0,0 +1,72 @@
+﻿/*=========================================
+
+▶️ 제목: 뮤탈리스크
+▶️ 풀이 여부: ❌ (시간초과)
+▶️ 풀이 시간: 60분
+▶️ 풀이 참고: X
+
+bfs가 아닌 dfs로 풀어서 그런지 시간초과가 나왔다. 
+완탐문제는 순열, 조합과 더불어 대부분 dfs, bfs로 푸는 것 같기도 하고... 아직 아리까리하다.
+
+=========================================*/
+
+#include <bits/stdc++.h>
+using namespace std;  
+
+const int INF=1e9;
+int N, a[5], cnt, ret=INF, dmg[]={9,3,1};
+vector<int> v;
+
+void go() {
+    bool flag=0;
+    vector<int> v2(v.size());
+    copy(v.begin(), v.end(), v2.begin());   // 전역변수 v로 순열을 만들고 재귀에 쓰이게 되면 무한루프에 빠지므로 지역변수로 카피해준다.
+
+    for (int i=0; i<N; ++i) {
+        if (a[i]>0) flag=1;
+    }
+
+    if (flag) {
+        queue<pair<int,int>> q;
+        do {
+            int i=0;
+            for (int idx : v2) {
+                if (a[idx]>0) {
+                    a[idx] -= dmg[i]; q.push({idx,i});
+                    ++i;
+                } 
+            } ++cnt;
+            
+            go();
+            
+            while (q.size()) {   // 백트래킹
+                auto it = q.front();
+                a[it.first] += dmg[it.second]; q.pop();
+            } --cnt;
+            
+        } while(next_permutation(v2.begin(), v2.end()));
+    }
+    else {
+        ret = min(ret, cnt); return;
+    }
+
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i=0; i<N; ++i) {
+        cin >> a[i];
+        v.push_back(i);
+    }
+
+    go();
+
+    cout << ret << '\n';
+
+    return 0;
+}
---
+++ problems/week3/b12869_2nd.cpp
@@ -0,0 +1,64 @@
+﻿/*=========================================
+
+▶️ 제목: 뮤탈리스크
+▶️ 풀이 여부: ✅
+▶️ 풀이 시간: 60분
+▶️ 풀이 참고: O
+
+"최단 거리"를 구한다는 관점에서 bfs로 풀어야한다고 한다. 
+최대 3개 요소로 되어있는 정점에서 공격 데미지들을 순열로 만들어 queue에 삽입된 순서대로 차례대로 빼고빼다보면 
+처음 모든 체력이 0으로 되는 레벨이 최단거리, 즉 공격해야하는 횟수의 최솟값인 것이다.
+삼차원 배열은 처음이지만 기본원리는 같으므로 익숙해지도록 하자.
+
+=========================================*/
+
+#include <bits/stdc++.h>
+using namespace std;  
+
+int N, visited[64][64][64], a[3];
+vector<vector<int>> dmg;
+
+struct A    // tuple보다 훨씬 낫다.
+{
+    int a,b,c;
+};
+
+queue<A> q;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i=0; i<N; ++i) {
+        cin >> a[i];
+    }
+
+    vector<int> d = {9,3,1};
+    do {dmg.push_back(d);} while (prev_permutation(d.begin(), d.end()));
+
+    visited[a[0]][a[1]][a[2]]=1;
+    q.push({a[0], a[1], a[2]});
+    while(q.size()) {
+        if (visited[0][0][0]) break;
+
+        int a = q.front().a;
+        int b = q.front().b;
+        int c = q.front().c;
+        q.pop();
+
+        for (int i=0; i<dmg.size(); ++i) {
+            int na = max(0, a - dmg[i][0]);
+            int nb = max(0, b - dmg[i][1]);
+            int nc = max(0, c - dmg[i][2]);
+            if (visited[na][nb][nc]) continue;
+            visited[na][nb][nc] = visited[a][b][c]+1;
+            q.push({na, nb, nc});
+        }
+    }
+
+    cout << visited[0][0][0] - 1 << '\n';
+
+    return 0;
+}
