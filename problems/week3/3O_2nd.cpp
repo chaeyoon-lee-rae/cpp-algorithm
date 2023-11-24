06124f5b5421784034b2dd214104bba48255f9af
Rachel Lee <engrrachel318@gmail.com>
2023-11-24 12:02:08 +0900

    refactor: change name

---
+++ problems/week3/3O_2nd.cpp
@@ -0,0 +1,69 @@
+﻿/*=========================================
+
+▶️ 제목: 사다리 조작
+▶️ 풀이 여부: ✅
+▶️ 풀이 시간: 60분
+▶️ 풀이 참고: X
+
+- 참고) long long으로 INF 구현 → const long long INF = 1e18;
+- combi 함수는 꼭 vector로 구현되지 않아도 된다
+    - “갯수”를 구하는 문제라면 counter 정수형으로 대체 가능
+- 또한 combi 함수는 2차원이라면 이중 for-loop을 사용할 수 있다
+    - 처음 combi 함수는 시작하고자하는 값에서 -1 작은 값에서 시작하지만
+    - 이 경우라면 시작할 때 시작하고자 하는 값 그대로 매개변수로 넘겨준다
+    - 2차 for-loop, j가 1로 시작하여 중복 여부가 있지만 visited로 그 경우는 건너뛴다
+- 마지막에 두가지 경우 중 하나를 출력해야할 시 삼항 연산자로 구현하면 간편하다
+
+=========================================*/
+
+#include <bits/stdc++.h>
+using namespace std;  
+
+const int INF = 1e6;
+int N, M, H, tempx, tempy, ret=INF;
+bool ladder[34][14];
+
+bool check() {
+    for (int i=1; i<=N; ++i) {
+        int y=1, x=i;
+        while (y<=H) {
+            if (ladder[y][x]) ++x;
+            else if (ladder[y][x-1]) --x;
+            ++y;
+        }
+        if (x!=i) return false;
+    }
+    return true;
+}
+
+void combi(int start, int cnt){ 
+    if (cnt > 3 || cnt >= ret) return;
+    if (check()) {ret = min(cnt, ret); return;}
+    for(int i = start; i<=H ; ++i){ 
+        for (int j=1; j<=N; ++j) {
+            if (ladder[i][j] || ladder[i][j-1] || ladder[i][j+1]) continue;
+            ladder[i][j] = 1;
+            combi(i, cnt+1);
+            ladder[i][j] = 0;
+        }
+    }
+
+    return; 
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M >> H;
+    for (int i=0; i<M; ++i) {
+        cin >> tempy >> tempx;
+        ladder[tempy][tempx]=1;
+    }
+
+    combi(1,0);
+    cout << ((ret==INF) ? -1 : ret) << '\n';
+
+    return 0;
+}
---
+++ problems/week3/3Q_2nd.cpp
@@ -0,0 +1,54 @@
+﻿/*=========================================
+
+▶️ 제목: 컴백홈
+▶️ 풀이 여부: ✅
+▶️ 풀이 시간: 20분
+▶️ 풀이 참고: O
+
+- go 함수 기저 사례에 1 또는 0을 반환하고 최종적으로 경우의 수를 다 더해줘 반환한다
+- BFS처럼 visited에 최단거리를 입력한다
+
+=========================================*/
+
+#include <bits/stdc++.h>
+using namespace std;  
+
+int R, C, K, dy[]={0,-1,0,1}, dx[]={1,0,-1,0}, visited[6][6];
+char a[6][6];
+
+int go(int y, int x) {
+    if (y==0 && x==C-1) {
+        if (visited[y][x]==K) return 1;
+        return 0;
+    }
+    int ret=0;
+    for (int i=0; i<4; ++i) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (ny<0||nx<0||ny>=R||nx>=C) continue;
+        if (!visited[ny][nx]&&a[ny][nx]!='T') {
+            visited[ny][nx]=visited[y][x]+1;
+            ret+=go(ny, nx);
+            visited[ny][nx]=0;
+        }
+    }
+    return ret;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> R >> C >> K;
+    for (int i=0; i<R; ++i) {
+        for (int j=0; j<C; ++j) {
+            cin >> a[i][j];
+        }
+    }
+
+    visited[R-1][0]=1;
+    cout << go(R-1,0) << '\n';    
+
+    return 0;
+}
