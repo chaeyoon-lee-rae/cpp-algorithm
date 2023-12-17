ba858e4d7ac7780756651c0128a9973b16e14ebe
Rachel Lee <engrrachel318@gmail.com>
2023-12-17 21:40:00 +0900

    feat: add 3C

---
+++ problems/week4/3C_1st.cpp
@@ -0,0 +1,72 @@
+﻿/*=========================================
+
+▶️ 제목: 게리맨더링
+▶️ 풀이 여부: ✅
+▶️ 풀이 시간: 60분
+▶️ 풀이 참고: △
+
+=========================================*/
+
+#include <bits/stdc++.h>
+using namespace std;  
+
+const int INF=1e6;
+int N, pop[11], popSum, ret=INF;
+bool visited[11], color[11];
+vector<int> v[11];
+
+void dfs(int here, int colored) {
+    visited[here]=1;
+    for (int there : v[here-1]) {
+        if (color[there]==colored && !visited[there]) dfs(there, colored);  // 처음에 같은 구역으로 나누는 것을 구현 안해서 틀림
+    }
+}
+
+void go(int node, vector<int> vec1, vector<int> vec2) {
+    if (node==N+1) {
+        if (!vec1.size()||!vec2.size()) return;
+        memset(visited, 0, sizeof(visited)); dfs(vec1[0],1); 
+        int numppl=0;
+        for (int val:vec1) {
+            if (!visited[val]) return;
+            numppl += pop[val-1]; 
+        }
+
+        memset(visited, 0, sizeof(visited)); dfs(vec2[0],0);
+        for (int val:vec2) if (!visited[val]) return;
+        ret = min(ret, abs(numppl-(popSum-numppl)));
+        return;
+    }
+
+    vec1.push_back(node); color[node]=1;
+    go(node+1, vec1, vec2);
+    vec1.pop_back(); color[node]=0;
+    vec2.push_back(node);
+    go(node+1, vec1, vec2);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i=0; i<N; ++i) {
+        cin >> pop[i];
+        popSum += pop[i];
+    }
+    for (int i=0; i<N; ++i) {
+        int tempCnt=0; cin >> tempCnt;
+        for (int j=0; j<tempCnt; ++j) {
+            int temp=0; cin >> temp;
+            v[i].push_back(temp);
+        }
+    }
+
+    vector<int> vec1, vec2;
+    go(1, vec1, vec2);
+    
+    cout << ((ret==INF) ? -1 : ret) << '\n';
+
+    return 0;
+}
---
+++ problems/week4/3C_fix.cpp
@@ -0,0 +1,63 @@
+﻿/*=========================================
+
+▶️ 제목: 게리맨더링
+▶️ 풀이 여부: ✅
+▶️ 풀이 시간: 60분
+▶️ 풀이 참고: O
+
+=========================================*/
+
+#include <bits/stdc++.h>
+using namespace std;  
+
+const int INF=1e6;
+int N, pop[11], ret=INF, idx1, idx2;
+bool visited[11], color[11];
+vector<int> v[11];
+
+pair<int,int> dfs(int here, int colored) {  // dfs로 여러가지 타입의 아웃풋 연습하기
+    visited[here]=1;
+    pair<int,int> ret = {1,pop[here]};
+    for (int there : v[here]) {
+        if (color[there]!=colored || visited[there]) continue;
+        pair<int,int> temp = dfs(there, colored);
+        ret.first += temp.first;
+        ret.second += temp.second;
+    }
+    return ret;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i=1; i<=N; ++i) {  // 처음에 1부터 시작하지 않아서 틀림
+        cin >> pop[i];
+    }
+    for (int i=1; i<=N; ++i) {
+        int tempCnt=0; cin >> tempCnt;
+        for (int j=0; j<tempCnt; ++j) {
+            int temp=0; cin >> temp;
+            v[i].push_back(temp);
+        }
+    }
+
+    for (int i=1; i<(1<<N)-1; ++i) {
+        memset(color, 0, sizeof(color));
+        memset(visited, 0, sizeof(visited));
+        idx1=-1, idx2=-1;
+        for (int j=0; j<N; ++j) {
+            if (i & (1<<j)) color[j+1]=1, idx1=j+1;
+            else idx2=j+1;
+        }
+        pair<int,int> res1 = dfs(idx1, 1);
+        pair<int,int> res2 = dfs(idx2, 0);
+        if (res1.first + res2.first == N) ret = min(ret, abs(res1.second-res2.second));
+    }
+    
+    cout << ((ret==INF) ? -1 : ret) << '\n';
+
+    return 0;
+}
