c7a43aa2f40d6f00c93bc681bc58d2a2c1cd3fe9
Chaeyoon Lee <engrrachel318@gmail.com>
2024-01-06 16:01:01 +0900

    [Gold III] Title: 종이 조각, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14391. 종이 조각/종이 조각.cc
+++ 백준/Gold/14391. 종이 조각/종이 조각.cc
@@ -1,59 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;  
 
-int N, M, a[5][5], vis[5][5], ret;
-
-int count() {
-    int sum=0;
-    for (int i=0; i<N; ++i) {
-        for (int j=0; j<M; ++j) {
-            string str="";
-            while (vis[i][j]==0 && j<M) {
-                str += a[i][j]+'0';
-                ++j;
-            }
-            sum += atoi(str.c_str());
-        }
-    }
-    for (int j=0; j<M; ++j) {
-        for (int i=0; i<N; ++i) {
-            string str="";
-            while (vis[i][j]==1 && i<N) {
-                str += a[i][j]+'0';
-                ++i;
-            }
-            sum += atoi(str.c_str());
-        }
-    }
-    return sum;
-}
-
-void go(int idx) {
-    if (idx==N*M) {
-        int sum = count();
-        ret = max(ret, sum);
-        return;
-    }
-    vis[idx/M][idx%M]=0;
-    go(idx+1);
-    vis[idx/M][idx%M]=1;
-    go(idx+1);
-}
+int N, M, a[5][5], ret;
+string s;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> N >> M;
+    scanf("%d %d", &N, &M);
     for (int i=0; i<N; ++i) {
-        string s; cin >> s;
         for (int j=0; j<M; ++j) {
-            a[i][j] = s[j]-'0';
+            scanf("%1d", &a[i][j]);
         }
     }
 
-    go(0);
+    for (int op=0; op<(1<<(N*M)); ++op) {
+        int sum=0;
+        for (int i=0; i<N; ++i) {
+            int num=0;
+            for (int j=0; j<M; ++j) {
+                int k=i*M+j;
+                if ((op&(1<<k))==0) num = num*10 + a[i][j];
+                else sum += num, num=0;
+            }
+            sum += num;     // 포함되지 않은 숫자 꼭 끝에 더해주기
+        }
+        for (int j=0; j<M; ++j) {
+            int num=0;
+            for (int i=0; i<N; ++i) {
+                int k=i*M+j;
+                if ((op&(1<<k))!=0) num = num*10 + a[i][j]; // Boolean 연산때에는 && 아닌 &임 (warning: converting the result of '<<' to a boolean;)
+                else sum += num, num=0;
+            }
+            sum += num;
+        }
+        ret = max(ret, sum);
+    }
+
     cout << ret << '\n';
 
     return 0;
