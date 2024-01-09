adc7c8cefd647506a0084c30c29567eb905cff49
Chaeyoon Lee <engrrachel318@gmail.com>
2024-01-09 16:08:06 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;  
+
+typedef long long ll;
+ll a, b, c;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    scanf("%lld %lld %lld", &a, &b, &c);
+    printf("%lld\n", a+b+c);
+
+    return 0;
+}
