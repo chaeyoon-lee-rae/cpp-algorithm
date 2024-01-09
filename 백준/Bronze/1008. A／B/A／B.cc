a494a011470cdfbd3eb826698fb1e2f768df1a1a
Chaeyoon Lee <engrrachel318@gmail.com>
2024-01-09 15:17:01 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;  
+
+double a, b;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    scanf("%lf %lf", &a, &b);
+    printf("%.10lf\n", a/b);
+
+    return 0;
+}
