cc17788b02539af052724c873705896e5e2d0594
Chaeyoon Lee <engrrachel318@gmail.com>
2024-01-10 22:06:52 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;  
+
+int N, a, b;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    while(scanf("%d %d", &a, &b)!=EOF) {
+        printf("%d\n", a+b);
+    }
+
+    return 0;
+}
