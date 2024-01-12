5e688faf919f93b362b9bc37ed32aa0b86675c80
Chaeyoon Lee <engrrachel318@gmail.com>
2024-01-12 13:12:56 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;  
+
+int N;
+char ch;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    scanf("%c", &ch);
+    printf("%d\n", ch);
+
+    return 0;
+}
