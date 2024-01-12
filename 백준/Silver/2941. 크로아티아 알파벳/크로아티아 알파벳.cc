46e41488dc64573754d9dc2aad5afcf965c3b180
Chaeyoon Lee <engrrachel318@gmail.com>
2024-01-12 16:27:19 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -4,6 +4,10 @@ using namespace std;
 string s, sub;
 int cnt;
 
+bool compLetters(string sub) {
+    return sub=="c="||sub=="c-"||sub=="d-"||sub=="lj"||sub=="nj"||sub=="s="||sub=="z=";
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -11,16 +15,9 @@ int main() {
 
     cin >> s;
     for (int i=0; i<s.size(); ++i) {
-        bool flag=0;
-        if (i+2<s.size()) {
-            sub = s.substr(i,3);
-            if (sub=="dz=") ++cnt, i+=2, flag=1;
-        }
-        if (!flag&&i+1<s.size()) {
-            sub = s.substr(i,2);
-            if (sub=="c="||sub=="c-"||sub=="d-"||sub=="lj"||sub=="nj"||sub=="s="||sub=="z=") ++cnt, ++i, flag=1;
-        }
-        if (!flag) ++cnt;
+        if (i+2<s.size()&&s.substr(i,3)=="dz=") ++cnt, i+=2;
+        else if (i+1<s.size()&&compLetters(s.substr(i,2))) ++cnt, ++i;
+        else ++cnt;
     }
     cout << cnt << '\n';
 
