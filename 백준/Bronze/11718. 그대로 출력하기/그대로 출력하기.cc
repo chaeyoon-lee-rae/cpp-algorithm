6a678b39de05c4ddeafb5bf08b6e9931774f0465
Chaeyoon Lee <engrrachel318@gmail.com>
2024-01-12 14:35:51 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;  
+
+int t;
+string s;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    while(getline(cin, s)) {
+        if (s.empty()) break;
+        cout << s << '\n';
+    }
+
+    return 0;
+}
