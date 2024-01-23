fe99b7ab65be162ea5c351b198944ab7196279c3
Chaeyoon Lee <engrrachel318@gmail.com>
2024-01-23 16:34:25 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;  
+
+typedef long long ll;
+ll N;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    cout << N*N << '\n';
+    cout << 2 << '\n';
+
+    return 0;
+}
