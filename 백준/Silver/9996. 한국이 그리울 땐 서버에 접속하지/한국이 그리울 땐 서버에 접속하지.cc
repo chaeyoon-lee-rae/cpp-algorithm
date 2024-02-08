8fe43c20ea8de1a209fb7b288c48c914e89bafd0
Chaeyoon Lee <engrrachel318@gmail.com>
2024-02-08 13:38:23 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;  
+
+int N;
+string org, start, last, temp;
+
+bool comp(string &temp) {
+    if (start.size()+last.size()<=temp.size()) {
+        return temp.substr(0,start.size())==start&&temp.substr(temp.size()-last.size(),last.size())==last;
+    } else return false;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> org;
+    int i;
+    for (i=0; i<org.size(); ++i) {
+        if (org[i]!='*') start+=org[i];
+        else {++i; break;}
+    }
+    last = org.substr(i, org.size()-i);
+
+    for (int i=0; i<N; ++i) {
+        cin >> temp;
+        cout << (comp(temp) ? "DA" : "NE") << '\n';
+    }
+
+    return 0;
+}
