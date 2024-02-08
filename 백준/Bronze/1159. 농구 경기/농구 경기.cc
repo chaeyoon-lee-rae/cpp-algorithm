3923b8f038c28fbc41aa1baacd253c728811cc94
Chaeyoon Lee <engrrachel318@gmail.com>
2024-02-08 12:54:32 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.cc
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -1,29 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;  
 
-int num;
-int arr[26];
-string name;
-string ret;
+int a[26], N, cnt;
+string s, ret;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> num;
-    for (int i=0; i<num; ++i) {
-        cin >> name;
-        arr[name[0] - 'a']++;
+    cin >> N;
+    for (int i=0; i<N; ++i) {
+        cin >> s;
+        ++a[s[0]-'a'];
+        cnt = max(cnt, a[s[0]-'a']);
     }
 
     for (int i=0; i<26; ++i) {
-        if (arr[i] >= 5) {
-            ret += (char)(i + 'a');
-        }
+        if (a[i]>=5) ret += i+'a';
     }
 
-    if (ret.size() == 0) cout << "PREDAJA" << '\n';
-    else cout << ret << '\n';
+    cout << (ret.empty() ? "PREDAJA" : ret) << '\n';
 
+    return 0;
 }
