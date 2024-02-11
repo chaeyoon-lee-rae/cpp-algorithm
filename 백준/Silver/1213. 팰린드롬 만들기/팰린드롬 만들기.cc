c9d84a4bab0093dd5636092133874257301286cf
Chaeyoon Lee <engrrachel318@gmail.com>
2024-02-11 15:06:08 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;  
+
+string start, mid, ret;
+int a[27];
+bool flag;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string s; cin >> s;
+    for (int i=0; i<s.size(); ++i) {
+        ++a[s[i]-'A'];
+    }
+
+    for (int i=0; i<26; ++i) {
+        if (a[i]==0) continue;
+        else {
+            start.append(a[i]/2,i+'A');
+            mid.append(a[i]%2,i+'A');
+        }
+    }
+    if (mid.size()>1) cout << "I'm Sorry Hansoo" << '\n';
+    else {
+        ret += start;
+        ret += mid;
+        reverse(start.begin(), start.end());
+        ret += start; cout << ret << '\n';
+    }
+
+    return 0;
+}
