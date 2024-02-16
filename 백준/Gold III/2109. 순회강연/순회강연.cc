cfc91beac87694da346edfb4031cef43cddad104
Chaeyoon Lee <engrrachel318@gmail.com>
2024-02-16 15:04:19 +0900

    [Gold III] Title: 순회강연, Time: 4 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Gold III/2109. 순회강연/순회강연.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;  
+
+int N, ret;
+vector<pair<int,int>> v;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i=0; i<N; ++i) {
+        int d, p; cin >> p >> d;
+        v.push_back({d,p});
+    }
+
+    sort(v.begin(), v.end());
+    for (int i=0; i<N; ++i) {
+        pq.push(v[i].second);
+        if (pq.size()>v[i].first) pq.pop();
+    }
+    while (pq.size()) {
+        ret += pq.top(); pq.pop();
+    }
+    cout << ret << '\n';
+
+    return 0;
+}
