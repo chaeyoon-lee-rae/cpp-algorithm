ff36d119f3424faf5474ddcb0e8b9cc94d1424b6
Chaeyoon Lee <engrrachel318@gmail.com>
2024-02-19 21:46:41 +0900

    [Bronze I] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;  
+
+int a, b, N, ret;
+
+int gcd(int a, int b) {
+    int r = a % b;
+    if (r==0) return b;
+    return gcd(b, r);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N; 
+    while(N--) {
+        cin >> a >> b;
+        int r = gcd(a, b);
+        ret = (a*b)/r;
+        cout << ret << '\n';
+    }
+
+    return 0;
+}
