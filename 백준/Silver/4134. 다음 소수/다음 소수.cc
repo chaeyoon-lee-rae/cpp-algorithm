338fdbc6ac461962fe126be19a37acd7ef0d3b0f
Chaeyoon Lee <engrrachel318@gmail.com>
2024-02-20 15:59:41 +0900

    [Silver IV] Title: 다음 소수, Time: 244 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.cc
+++ 백준/Silver/4134. 다음 소수/다음 소수.cc
@@ -9,7 +9,7 @@ bool isPrime(ll &num) {
     if (num==2||num==3) return true;
     if (num%2==0 || num%3==0) return false;
     for (ll i=5; i*i<=num; ++i) {
-        if (num%i==0 || num % (i+2)==0) return false;
+        if (num%i==0) return false;
     }
     return true;
 }
