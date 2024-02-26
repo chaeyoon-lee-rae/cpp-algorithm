92fbc01aed85a20f09a34957d2b0c060500b7d90
Chaeyoon Lee <engrrachel318@gmail.com>
2024-02-26 16:53:47 +0900

    [Silver I] Title: 스타트와 링크, Time: 100 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -1,27 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;  
 
-const int INF = 1e9;
-int N, s[21][21], ret=INF;
+const int INF=1e9;
+int N, ret=INF, s[21][21];
+vector<int> v;
+bool vis[21];
 
-int calc(vector<int> v) {
-    int score=0;
-    for (int i=0; i<v.size()-1; ++i) {
-        for (int j=i+1; j<v.size(); ++j) {
-            score+=s[v[i]][v[j]];
-            score+=s[v[j]][v[i]];
+int go(vector<int> &a, vector<int> &b) {
+    int score1=0, score2=0;
+    for (int i=0; i<a.size()-1; ++i) {
+        for (int j=i+1; j<a.size(); ++j) {
+            score1 += s[a[i]][a[j]] + s[a[j]][a[i]];
+            score2 += s[b[i]][b[j]] + s[b[j]][b[i]];
         }
     }
-    return score;
+    return abs(score1 - score2);
 }
 
-void go(int num) {
-    vector<int> v1, v2;
-    for (int i=0; i<N; ++i) {
-        if ((1<<i)&num) v1.push_back(i);
-        else v2.push_back(i);
+void combi(int start, int cnt) {
+    if (cnt==N/2) {
+        vector<int> v1, v2;
+        for (int i=0; i<N; ++i) {
+            if (vis[i]) v1.push_back(i);
+            else v2.push_back(i);
+        }
+        ret = min(ret, go(v1, v2));
+        return;
     }
-    ret = min(ret, abs(calc(v1) - calc(v2)));
+    for (int i=start+1; i<N; ++i) {
+        vis[i]=1;
+        combi(i, cnt+1);
+        vis[i]=0;
+    }
     return;
 }
 
@@ -36,13 +46,8 @@ int main() {
             cin >> s[i][j];
         }
     }
-    for (int i=0; i<(1<<N); ++i) {
-        int cnt = 0;
-        for (int j=0; j<N; ++j) {
-            if ((1<<j)&i) ++cnt;
-        }
-        if (cnt==N/2) go(i);
-    }
+
+    combi(-1, 0);
     cout << ret << '\n';
 
     return 0;
