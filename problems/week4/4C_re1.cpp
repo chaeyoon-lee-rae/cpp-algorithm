4c143c8eabd3b4d31090f7bb68c4c36881624a93
Rachel Lee <engrrachel318@gmail.com>
2024-03-05 23:21:41 +0900

    fix: fix typo

--- problems/week4/4C_re1.cpp
+++ problems/week4/4C_re1.cpp
@@ -2,7 +2,7 @@
 using namespace std;  
 
 const int INF=1e6;
-int N, pop[11], ret=INF, gary[11];
+int N, pop[11], ret=INF, gerry[11];
 vector<vector<int>> v;
 bool visited[11];
 
@@ -15,14 +15,14 @@ int calcPop(vector<int> v) {
 void go(int here, int num) {
     visited[here]=1;
     for (int val:v[here]) {
-        if (!visited[val]&&gary[val]==num) go(val, num);
+        if (!visited[val]&&gerry[val]==num) go(val, num);
     }
     return;
 }
 
 bool check(vector<int> &v) {
     memset(visited, 0, sizeof(visited));
-    go(v[0], gary[v[0]]);
+    go(v[0], gerry[v[0]]);
     for (int val: v) {
         if (visited[val]==0) return false;
     }
@@ -50,9 +50,9 @@ int main() {
 
     for (int i=1; i<(1<<N)-1; ++i) {
         vector<int> v1, v2;
-        memset(gary, 0, sizeof(gary));
+        memset(gerry, 0, sizeof(gerry));
         for (int j=0; j<N; ++j) {
-            if (i&(1<<j)) v1.push_back(j), gary[j]=1;
+            if (i&(1<<j)) v1.push_back(j), gerry[j]=1;
             else v2.push_back(j);
         }
         if (check(v1)&&check(v2)) {
