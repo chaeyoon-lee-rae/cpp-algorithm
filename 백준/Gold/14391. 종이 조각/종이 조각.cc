eee6af874cd8a11c6f631a8090408d9d6797f0d4
Chaeyoon Lee <engrrachel318@gmail.com>
2024-03-11 20:18:37 +0900

    [Gold III] Title: 종이 조각, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14391. 종이 조각/종이 조각.cc
+++ 백준/Gold/14391. 종이 조각/종이 조각.cc
@@ -2,7 +2,40 @@
 using namespace std;  
 
 int N, M, a[5][5], ret;
-string s;
+bool b[5][5];
+
+int calc(int &num) {
+    int sum=0;
+    for (int i=0; i<N; ++i) {
+        int temp=0;
+        for (int j=0; j<M; ++j) {
+            if (num&(1<<(M*i+j))) temp=temp*10+a[i][j];
+            else sum+=temp, temp=0;
+        }
+        sum+=temp;
+    }
+    for (int j=0; j<M; ++j) {
+        int temp=0;
+        for (int i=0; i<N; ++i) {
+            if ((num&(1<<(M*i+j)))==0) temp=temp*10+a[i][j];
+            else sum+=temp, temp=0;
+        }
+        sum+=temp;
+    }
+    return sum;
+}
+
+void comb(int num, int cnt) {
+    if (cnt==N*M) {
+        ret = max(ret, calc(num));
+        return;
+    }
+
+    comb(num, cnt+1);
+    comb(num|(1<<cnt), cnt+1);
+
+    return;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -16,30 +49,8 @@ int main() {
         }
     }
 
-    for (int op=0; op<(1<<(N*M)); ++op) {
-        int sum=0;
-        for (int i=0; i<N; ++i) {
-            int num=0;
-            for (int j=0; j<M; ++j) {
-                int k=i*M+j;
-                if ((op&(1<<k))==0) num = num*10 + a[i][j];
-                else sum += num, num=0;
-            }
-            sum += num;     // 포함되지 않은 숫자 꼭 끝에 더해주기
-        }
-        for (int j=0; j<M; ++j) {
-            int num=0;
-            for (int i=0; i<N; ++i) {
-                int k=i*M+j;
-                if ((op&(1<<k))!=0) num = num*10 + a[i][j]; // Boolean 연산때에는 && 아닌 &임 (warning: converting the result of '<<' to a boolean;)
-                else sum += num, num=0;
-            }
-            sum += num;
-        }
-        ret = max(ret, sum);
-    }
-
-    cout << ret << '\n';
+    comb(0, 0);
+    printf("%d\n", ret);
 
     return 0;
 }
