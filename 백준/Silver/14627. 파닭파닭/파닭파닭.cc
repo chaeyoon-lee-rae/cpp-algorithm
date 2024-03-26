46493866c4ba8a17d43b1b6709e88c36b45e8f35
Chaeyoon Lee <engrrachel318@gmail.com>
2024-03-27 08:28:37 +0900

    [Silver II] Title: 파닭파닭, Time: 232 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/14627. 파닭파닭/파닭파닭.cc
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.cc
@@ -2,11 +2,12 @@
 using namespace std;  
 
 typedef long long ll;
-const ll INF=1e9+1;
-ll s, c, l[1000001], lo, hi, ret, sum;
+const int INF=1e9+1;
+int s, c, l[1000001], lo, hi, ret;
+ll sum;
 
-ll check(ll mid) {
-    ll cnt=0;
+int check(int mid) {
+    int cnt=0;
     for (int i=0; i<s; ++i) {
         cnt+=l[i]/mid;
     }
@@ -20,20 +21,20 @@ int main() {
 
     cin >> s >> c;
     for (int i=0; i<s; ++i) {
-        cin >> l[i];
+        cin >> l[i]; 
         sum+=l[i];
     }
 
     lo=1, hi=INF;
     while(lo<=hi) {
-        ll mid = (lo+hi)/2;
-        ll cnt=check(mid);
+        int mid = (lo+hi)/2;
+        int cnt=check(mid);
         if (cnt>=c) {
-            ret = mid;
+            ret=mid;
             lo=mid+1;
         } else hi=mid-1;
     }
-    cout << sum-ret*c << '\n';
+    cout << sum-(ll)ret*c << '\n';
 
     return 0;
 }
