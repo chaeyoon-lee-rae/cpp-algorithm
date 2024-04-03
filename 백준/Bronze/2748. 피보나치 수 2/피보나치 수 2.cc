c0a4a197e770b58cf97fd0dd94adb9df4a0bf5c5
Chaeyoon Lee <engrrachel318@gmail.com>
2024-04-03 18:55:51 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -4,20 +4,19 @@ using namespace std;
 typedef long long ll;
 ll dp[91]; int n;
 
-ll fibo(int num) {
-    if (num==0|num==1) return num;
-    ll &ret=dp[num];
-    if(ret) return ret;
-    return ret=fibo(num-1)+fibo(num-2);
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
+    dp[1]=1;
+    for (int i=2; i<=90; ++i) {
+        dp[i] = dp[i-1]+dp[i-2];
+    }
+
     cin >> n;
-    cout << fibo(n) << '\n';
+    cout << dp[n] << '\n';
+
 
     return 0;
 }
