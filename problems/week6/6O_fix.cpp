f70b1ac4bf3175f3cd1080c3fec7363bd6115c65
Rachel Lee <engrrachel318@gmail.com>
2024-04-09 23:16:14 +0900

    feat: add 6O

---
+++ problems/week6/6O_fix.cpp
@@ -0,0 +1,27 @@
+﻿#include <bits/stdc++.h>
+using namespace std;  
+
+int lis[101], n, x, y, len;
+pair<int,int> a[101];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i=0; i<n; ++i) {
+        cin >> x >> y;
+        a[i]={x,y};
+    }
+    sort(a,a+n);
+
+    for (int i=0; i<n; ++i) {
+        auto it=lower_bound(lis,lis+len,a[i].second);
+        if (*it==0) ++len;
+        *it=a[i].second;
+    }
+    cout << n-len << '\n';
+
+    return 0;
+}
