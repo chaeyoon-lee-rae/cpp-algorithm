8d412ff645a9dce351a25a349e5a5a52e12090a6
Chaeyoon Lee <engrrachel318@gmail.com>
2024-04-30 21:20:59 +0900

    [Gold V] Title: 동전 2, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -2,8 +2,7 @@
 using namespace std;  
 
 const int INF=1e6;
-int dp[10001], n, k, num;
-vector<int> coin;
+int n, dp[10001], k, num;
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -11,18 +10,13 @@ int main() {
     cout.tie(NULL);
 
     fill(dp, dp+10001, INF);
+    dp[0]=0;
+
     cin >> n >> k;
     for (int i=0; i<n; ++i) {
-        int temp; cin >> temp;
-        coin.push_back(temp);
-    }
-    sort(coin.begin(), coin.end());
-    coin.erase(unique(coin.begin(), coin.end()), coin.end());
-
-    for (int num:coin) {
-        for (int i=num; i<=k; ++i) {
-            if (!(i%num)) dp[i]=min(dp[i],i/num);
-            if (dp[i-num]!=INF) dp[i]=min(dp[i],dp[i-num]+1);
+        cin >> num;
+        for (int j=num; j<=k; ++j) {
+            dp[j]=min(dp[j],dp[j-num]+1);
         }
     }
     cout << (dp[k]==INF ? -1 : dp[k]) << '\n';
