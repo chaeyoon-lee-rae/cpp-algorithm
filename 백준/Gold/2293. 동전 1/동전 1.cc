a4f3e2439c3660e9734e63d3e6dfc1ad8223285f
Chaeyoon Lee <engrrachel318@gmail.com>
2024-04-30 21:46:57 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -1,22 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;  
 
-int dp[10001], coin[101], n, k, num;
+int dp[10001], n, k, num;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> n >> k;
     dp[0]=1;
+    cin >> n >> k;
     for (int i=0; i<n; ++i) {
         cin >> num;
-        for (int i=num; i<=k; ++i) {
-            dp[i]+=dp[i-num];
+        for (int j=num; j<=k; ++j) {
+            dp[j]+=dp[j-num];
         }
     }
-
     cout << dp[k] << '\n';
 
     return 0;
