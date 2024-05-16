16e97204350c929d77476b13c2b4fdd4da95b390
Chaeyoon Lee <engrrachel318@gmail.com>
2024-05-16 19:00:57 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 40 ms, Memory: 8144 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
@@ -1,19 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;  
 
+bool dp[2501][2501];
+int dp2[2501];
 string s;
-int dp[2501][2501], dp2[2501];
-const int INF=1e6;
-
-int go(int idx) {
-    if (idx==s.size()) return 0;
-    int &ret=dp2[idx];
-    if(ret!=INF) return ret;
-    for (int i=1; i+idx<=s.size(); ++i) {
-        if(dp[idx][i]) ret=min(ret,go(idx+i)+1);
-    }
-    return ret;
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -21,22 +11,32 @@ int main() {
     cout.tie(NULL);
 
     cin >> s;
-    for (int i=0; i<s.size(); ++i) {
-        dp[i][1]=1;
+    const int n = s.size();
+    // fill dp
+    for (int i=0; i<n; ++i) dp[i][i]=1;
+
+    for (int i=0; i<n-1; ++i) {
+        if (s[i]==s[i+1]) dp[i][i+1]=1;
     }
-    for (int i=0; i<s.size()-1; ++i) {
-        if(s[i]==s[i+1]) dp[i][2]=1;
-    }
-    for (int len=3; len<=s.size(); ++len) {
-        for (int i=0; i+len<=s.size(); ++i) {
-            if (s[i]==s[i+len-1]&&dp[i+1][len-2]) {
-                dp[i][len]=1;
-            }
+
+    for (int len=2; len<n; ++len) {
+        for (int i=0; len+i<n; ++i) {
+            if (s[i]==s[i+len]&&dp[i+1][i+len-1])
+                dp[i][i+len]=1;
         }
     }
 
-    fill(dp2, dp2+2501, INF);
-    cout << go(0) << '\n';
+    // fill dp2
+    for (int i=0; i<n; ++i) {
+        if (dp[0][i]) dp2[i]=1;
+        else {
+            dp2[i]=i+1;
+            for (int j=1; j<=i; ++j)
+                if (dp[j][i]) dp2[i]=min(dp2[i],dp2[j-1]+1);
+        }
+    }
+
+    cout << dp2[n-1] << '\n';
 
     return 0;
 }
