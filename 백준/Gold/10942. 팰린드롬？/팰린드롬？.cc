760ace6befa20032bec27f1d039d638bdcedfb75
Chaeyoon Lee <engrrachel318@gmail.com>
2024-05-16 10:36:32 +0900

    [Gold IV] Title: 팰린드롬?, Time: 236 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -10,21 +10,16 @@ int main() {
     cout.tie(NULL);
 
     cin >> n;
-    for (int i=1; i<=n; ++i) {
-        cin >> a[i];
-    }
+    for (int i=1; i<=n; ++i) cin >> a[i];
 
-    for (int i=1; i<=n; ++i) {
-        dp[i][i]=1;
-    }
-    for (int i=1; i<=n-1; ++i) {
+    for (int i=1; i<=n; ++i) dp[i][i]=1;
+
+    for (int i=1; i<=n-1; ++i)
         if (a[i]==a[i+1]) dp[i][i+1]=1;
-    }
-    for (int len=2; len<n; ++len) {
-        for (int i=1; i<=n-len; ++i) {
+
+    for (int len=2; len<n; ++len)
+        for (int i=1; i<=n-len; ++i)
             if (a[i]==a[i+len]&&dp[i+1][i+len-1]) dp[i][i+len]=1;
-        }
-    }
 
     cin >> m;
     for (int i=0; i<m; ++i) {
