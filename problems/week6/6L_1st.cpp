9b02a2d087bcd1113784529721878fea5f8b75c7
Rachel Lee <engrrachel318@gmail.com>
2024-06-03 23:52:30 +0900

    feat: add 6L

---
+++ problems/week6/6L_1st.cpp
@@ -0,0 +1,25 @@
+﻿#include <bits/stdc++.h>
+using namespace std;  
+
+int N;
+double a, temp, ret;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    scanf("%d", &N);
+    temp=1;
+    for (int i=0; i<N; ++i) {
+        scanf("%lf", &a);
+        temp*=a;
+        ret=max(ret,temp);
+        if (temp<1.0f) {
+            temp=1;
+        }
+    }
+    printf("%.3lf\n", ret);
+
+    return 0;
+}
