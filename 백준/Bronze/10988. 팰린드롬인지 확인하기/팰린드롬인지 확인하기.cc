410372eeb8269fb8d64995f146c9b5ebd595782e
Chaeyoon Lee <engrrachel318@gmail.com>
2024-06-20 21:26:58 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,20 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;  
 
-string s;
+char s[101];
+
+bool checkPalin(int l, int r) {
+    while(l<=r) {
+        if (s[l]!=s[r]) return false;
+        ++l, --r;
+    }
+    return true;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> s;
-
-    string temp = s;
-    reverse(temp.begin(), temp.end());
-
-    if (temp == s) cout << 1 << '\n';
-    else cout << 0 << '\n';
+    scanf("%s", s);
+    int len=0;
+    while(1) {
+        if (s[len]==0) break;
+        ++len;
+    }
+    printf("%d\n", checkPalin(0, len-1));
 
     return 0;
 }
