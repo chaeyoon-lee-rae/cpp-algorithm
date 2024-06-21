2e14e64d7303036ae3e3906a505451f3bc4d4131
Chaeyoon Lee <engrrachel318@gmail.com>
2024-06-21 22:37:52 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -1,25 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;  
 
-int K, N, ret;
+int t, n;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> K;
-    while(K--) {
-        cin >> N; ret=1;
-        unordered_map<string, int> mp;
-        for (int i=0; i<N; ++i) {
-            string s1, s2;
-            cin >> s1 >> s2;
+    cin >> t;
+    while(t--) {
+        map<string,int> mp;
+        cin >> n; int ret = 1;
+        for (int i=0; i<n; ++i) {
+            string s1, s2; cin >> s1 >> s2;
             ++mp[s2];
         }
         for (auto it:mp) ret *= it.second+1;
-        --ret;
-        cout << ret << '\n';
+        cout << --ret << '\n';
     }
 
     return 0;
