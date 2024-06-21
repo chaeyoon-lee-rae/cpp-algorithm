6e373bb7f642271dcaac08ae47de382b538366b6
Chaeyoon Lee <engrrachel318@gmail.com>
2024-06-21 19:59:25 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,26 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;  
 
-string s, ret;
+char s[101];
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    getline(cin,s);
-    for (int i=0; i<s.size(); ++i) {
+    cin.getline(s, 101);
+    int length = strlen(s);
+    for (int i=0; i<length; ++i) {
         if (s[i]>='A'&&s[i]<='Z') {
-            if (s[i]+13 > 'Z') ret+=(s[i]+13)%'Z'+'A'-1;
-            else ret+=(s[i]+13);
+            s[i]=((s[i]-'A')+13)%26 + 'A';
+        } else if (s[i]>='a'&&s[i]<='z') {
+            s[i]=((s[i]-'a')+13)%26 + 'a';
         }
-        else if (s[i]>='a'&&s[i]<='z') {
-            if (s[i]+13 > 'z') ret+=(s[i]+13)%'z'+'a'-1;
-            else ret+=(s[i]+13);
-        }
-        else ret += s[i];
     }
-    cout<< ret << '\n';
+    cout << s << endl;
 
     return 0;
 }
