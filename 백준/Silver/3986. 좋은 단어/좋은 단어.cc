12153a464eec6ddf76af8d0e6b9ef3a0c8c7dca5
Chaeyoon Lee <engrrachel318@gmail.com>
2024-06-26 23:19:23 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2252 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -2,25 +2,26 @@
 using namespace std;  
 
 int t, cnt; 
-string str;
+char str[100001];
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> t;
+    scanf("%d", &t);
     while(t--) {
         stack<char> stk;
-        cin >> str;
-        for (char ch:str) {
-            if (stk.size()&&stk.top()==ch)
+        scanf("%s", str);
+        int len = strlen(str);
+        for (int i=0; i<len; ++i) {
+            if (stk.size()&&stk.top()==str[i])
                 stk.pop();
-            else stk.push(ch);
+            else stk.push(str[i]);
         }
         if (stk.empty()) ++cnt;
     }
-    cout << cnt << '\n';
+    printf("%d\n", cnt);
 
     return 0;
 }
