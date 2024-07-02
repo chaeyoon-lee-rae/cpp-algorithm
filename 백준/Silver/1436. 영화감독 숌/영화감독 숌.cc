0a65f3bac43088ae64975c56c445ef43fa8e62de
Chaeyoon Lee <engrrachel318@gmail.com>
2024-07-03 01:14:28 +0900

    [Silver V] Title: 영화감독 숌, Time: 8 ms, Memory: 2536 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -11,18 +11,17 @@ int main() {
 
     cin >> N;
 
-    for (int i=0; i<pow(10,4); i++) {
+    for (int i=0; i<10000; i++) {
         string s = "000" + to_string(i);
         string a = s.substr(s.size()-4, 4);
         for (int j=0; j<=4; j++) {
-            string temp = a.substr(0, 4-j) + "666" + a.substr(4-j,j);
+            string temp = a.substr(0, 4-j) + "666" + a.substr(4-j);
             v.push_back(stoi(temp));
         }
     }
 
     sort(v.begin(), v.end());
     v.erase(unique(v.begin(), v.end()), v.end());
-
     cout << v[N-1] << '\n';
 
     return 0;
