d3e7c4b80e42b491000600b8595941646be459d7
Chaeyoon Lee <engrrachel318@gmail.com>
2024-08-27 18:35:55 +0900

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -1,43 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;  
 
-const int INF=-1e9;
-int N, ret=INF; char ch;
-vector<int> num;
-vector<char> oper;
+int n, number_n, ret=-1e9, number[10];
+char op[10];
 
-int calc(int idx, int num1, int num2) {
-    if (oper[idx]=='+') return num1+num2;
-    if (oper[idx]=='-') return num1-num2;
-    if (oper[idx]=='*') return num1*num2;
-    return 0;
+int calc(int op_idx, int a, int b) {
+	if (op[op_idx]=='*') return a * b;
+	if (op[op_idx]=='+') return a + b;
+	if (op[op_idx]=='-') return a - b;
+	return 0;
 }
 
-void go(int here, int sum) {
-    if (here==num.size()-1) {ret=max(ret,sum); return;}
+void go(int idx, int num) {
+	if (idx==number_n-1) {
+		ret = max(ret, num);
+		return;
+	}
 
-    go(here+1, calc(here,sum,num[here+1]));
-    if (here+2 <= num.size()-1) {
-        int temp=calc(here+1,num[here+1],num[here+2]);
-        go(here+2, calc(here,sum,temp));
-    }
+	go(idx+1, calc(idx, num, number[idx+1]));
+	if (idx+2<=number_n-1) {
+		int temp = calc(idx+1, number[idx+1], number[idx+2]);
+		go(idx+2, calc(idx, num, temp));
+	}
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    cin >> N;
-    for (int i=0; i<N; ++i) {
-        cin >> ch;
-        if (i%2==0) num.push_back(ch-'0');
-        if (i%2==1) oper.push_back(ch);
-    }
+	cin >> n; char ch;
+	for (int i=0; i<n; ++i) {
+		cin >> ch;
+		if (i%2) op[i/2] = ch;
+		else number[i/2] = ch - '0';
+	}
 
-    go(0, num[0]);
+	number_n = n/2 + 1;
 
-    cout << ret << '\n';
+	go(0, number[0]);
+	cout << ret << '\n';
 
-    return 0;
+	return 0;
 }
