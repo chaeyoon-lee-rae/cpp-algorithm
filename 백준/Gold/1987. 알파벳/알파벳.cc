5a6db1f3416c2a81d359bf848711752844983375
Chaeyoon Lee <engrrachel318@gmail.com>
2024-09-16 00:43:11 +0900

    [Gold IV] Title: 알파벳, Time: 344 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -3,19 +3,16 @@ using namespace std;
 
 int r, c, ret, dy[]={0,1,0,-1}, dx[]={1,0,-1,0};
 char a[21][21];
-bool visChar[27], visited[21][21];
+bool visChar[27];
 
 void dfs(int y, int x, int cnt) {
 	ret = max(ret, cnt);
 	for (int i=0; i<4; ++i) {
 		int ny = y + dy[i];
 		int nx = x + dx[i];
-		if (ny<0||nx<0||ny>=r||nx>=c) continue;
-		if (visited[ny][nx]||visChar[a[ny][nx]-'A']) continue;
-		visited[ny][nx] = 1;
+		if (ny<0||nx<0||ny>=r||nx>=c||visChar[a[ny][nx]-'A']) continue;
 		visChar[a[ny][nx]-'A'] = 1;
 		dfs(ny,nx,cnt+1);
-		visited[ny][nx] = 0;
 		visChar[a[ny][nx]-'A'] = 0;
 	}
 }
@@ -26,15 +23,10 @@ int main() {
 	cout.tie(NULL);
 
 	cin >> r >> c;
-	string s;
-	for (int i=0; i<r; ++i) {
-		cin >> s;
-		for (int j=0; j<c; ++j) {
-			a[i][j] = s[j];
-		}
-	}
+	for (int i=0; i<r; ++i)
+		for (int j=0; j<c; ++j)
+			cin >> a[i][j];
 
-	visited[0][0]=1;
 	visChar[a[0][0]-'A']=1;
 	dfs(0,0,1);
 	cout << ret << '\n';
