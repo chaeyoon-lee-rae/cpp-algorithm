bd1d104d369c34ebbfd79908a20a2eb600ee8f64
Chaeyoon Lee <engrrachel318@gmail.com>
2024-09-16 00:39:39 +0900

    [Gold IV] Title: 알파벳, Time: 404 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,41 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;  
 
-int R, C, vis, y, x, dy[]={0, -1, 0, 1}, dx[]={1, 0, -1, 0}, a[21][21], ret;
-queue<pair<int,int>> q;
+int r, c, ret, dy[]={0,1,0,-1}, dx[]={1,0,-1,0};
+char a[21][21];
+bool visChar[27], visited[21][21];
 
 void dfs(int y, int x, int cnt) {
-    ret = max(ret, cnt);
-
-    for (int i=0; i<4; ++i) {
-        int ny=y+dy[i];
-        int nx=x+dx[i];
-        if (ny<0||ny>=R||nx<0||nx>=C) continue;
-        if (!(vis & (1<<a[ny][nx]))) {
-            vis |= (1<<a[ny][nx]);
-            dfs(ny,nx,cnt+1);
-            vis &= ~(1<<a[ny][nx]);
-        }
-    }
+	ret = max(ret, cnt);
+	for (int i=0; i<4; ++i) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (ny<0||nx<0||ny>=r||nx>=c) continue;
+		if (visited[ny][nx]||visChar[a[ny][nx]-'A']) continue;
+		visited[ny][nx] = 1;
+		visChar[a[ny][nx]-'A'] = 1;
+		dfs(ny,nx,cnt+1);
+		visited[ny][nx] = 0;
+		visChar[a[ny][nx]-'A'] = 0;
+	}
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    cin >> R >> C;
-    for (int i=0; i<R; ++i) {
-        for (int j=0; j<C; ++j) {
-            char temp;
-            cin >> temp;
-            a[i][j]=temp-'A';
-        }
-    }
+	cin >> r >> c;
+	string s;
+	for (int i=0; i<r; ++i) {
+		cin >> s;
+		for (int j=0; j<c; ++j) {
+			a[i][j] = s[j];
+		}
+	}
 
-    vis |= (1<<a[0][0]);
-    dfs(0,0,1);
-    cout << ret << '\n';
+	visited[0][0]=1;
+	visChar[a[0][0]-'A']=1;
+	dfs(0,0,1);
+	cout << ret << '\n';
 
-    return 0;
+	return 0;
 }
