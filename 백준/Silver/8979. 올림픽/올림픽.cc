3725065346067961e7f3f54664ec2201c8dc90ab
Chaeyoon Lee <engrrachel318@gmail.com>
2024-09-23 12:05:34 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2168 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -2,21 +2,44 @@
 #include <map>
 using namespace std;
 
-int n, k, a[1001], ret;
-map<int, int, greater<int>> mp;
+struct medal {
+    int g, s, b;
+    bool operator < (const medal& temp) const {
+        if (g != temp.g) return g > temp.g;
+        else if (s != temp.s) return s > temp.s;
+        else return b > temp.b;
+    }
+
+    bool operator == (const medal& temp) const {
+        return g == temp.g && s == temp.s && b == temp.b;
+    }
+
+    bool operator != (const medal& temp) const {
+        return !(*this == temp);
+    }
+};
+
+struct medalCmp {
+    bool operator() (const medal& l, const medal& r) const {
+        return l < r;
+    }
+};
+
+int n, k, ret;
+medal a[1001];
+map<medal, int, medalCmp> mp;
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie();
-    cout.tie();
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
     cin >> n >> k;
     int c, g, s, b;
     for (int i = 0; i < n; ++i) {
         cin >> c >> g >> s >> b;
-        int sum = g * 100 + s * 10 + b;
-        ++mp[sum];
-        a[c] = sum;
+        ++mp[{g, s, b}];
+        a[c] = { g, s, b };
     }
 
     for (auto it = mp.begin(); it != mp.end(); ++it) {
