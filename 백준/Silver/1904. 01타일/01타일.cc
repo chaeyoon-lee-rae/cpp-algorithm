47407b4e4c7c3e9a7653bc1c9e1700c52ea05ed8
Chaeyoon Lee <engrrachel318@gmail.com>
2024-09-26 14:04:28 +0900

    [Silver III] Title: 01타일, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+const int mod = 15746;
+int dp[1000001], n;
+
+int main() {
+    cin >> n;
+
+    dp[0] = 1; dp[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+        dp[i] = ((dp[i - 1] % mod) + (dp[i - 2] % mod)) % mod;
+    }
+
+    cout << dp[n] << "\n";
+
+    return 0;
+}
