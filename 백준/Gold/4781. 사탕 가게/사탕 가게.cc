25888375e371827ab2e43a58c1e5a15c0792f933
Chaeyoon Lee <engrrachel318@gmail.com>
2024-10-02 15:37:07 +0900

    [Gold IV] Title: 사탕 가게, Time: 184 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/4781. 사탕 가게/사탕 가게.cc
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.cc
@@ -1,26 +1,26 @@
-#include <bits/stdc++.h>
-using namespace std;  
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
 
-int dp[10001], n, cal, d1, d2;
+int cal, n, budget, temp1, temp2, dp[10001];
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    while (true) {
+        scanf("%d %d.%d", &n, &temp1, &temp2);
 
-    while(true) {
-        scanf("%d %d.%d", &n, &d1, &d2);
-        if (n==0) break;
-        int limit=d1*100+d2;
+        if (n == 0) break;
+
         memset(dp, 0, sizeof(dp));
-        for (int i=0; i<n; ++i) {
-            scanf("%d %d.%d", &cal, &d1, &d2);
-            int cost=d1*100+d2;
-            for (int j=cost; j<=limit; ++j) {
-                dp[j]=max(dp[j], dp[j-cost]+cal);
-            }
+
+        budget = temp1 * 100 + temp2;
+        for (int i = 0; i < n; ++i) {
+            scanf("%d %d.%d", &cal, &temp1, &temp2);
+            int cost = temp1 * 100 + temp2;
+            for (int i = cost; i <= budget; ++i)
+                dp[i] = max(dp[i], dp[i - cost] + cal);
         }
-        printf("%d\n", dp[limit]);
+        printf("%d\n", dp[budget]);
     }
 
     return 0;
