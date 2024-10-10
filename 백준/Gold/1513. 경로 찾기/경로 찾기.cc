4b2a9b83ef070ea3dfe33161ea5358467e5763c8
Chaeyoon Lee <engrrachel318@gmail.com>
2024-10-10 13:55:12 +0900

    [Gold II] Title: 경로 찾기, Time: 40 ms, Memory: 28456 KB -BaekjoonHub

--- 백준/Gold/1513. 경로 찾기/경로 찾기.cc
+++ 백준/Gold/1513. 경로 찾기/경로 찾기.cc
@@ -1,40 +1,105 @@
-#include <bits/stdc++.h>
-using namespace std;  
+#include <iostream>
+using namespace std;
 
-const int d=1000007;
-int a[51][51], dp[51][51][51][51], n, m, c, y, x;
-
-int go(int y, int x, int cnt, int pre) {
-    if (y>n||x>m) return 0;
-    if (y==n&&x==m) {
-        if (cnt==0&&a[y][x]==0) return 1;
-        if (cnt==1&&a[y][x]>pre) return 1;
-        return 0;
-    }
-    int &ret=dp[y][x][cnt][pre];
-    if (~ret) return ret;
-    ret=0;
-    if (a[y][x]>pre) {
-        ret=(go(y,x+1,cnt-1,a[y][x])+go(y+1,x,cnt-1,a[y][x]))%d;
-    } else if (a[y][x]==0) {
-        ret=(go(y,x+1,cnt,pre)+go(y+1,x,cnt,pre))%d;
-    }
-    return ret;
-}
+const int mod = 1000007;
+int a[51][51], dp[51][51][51][51], n, m, c, x, y;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-    memset(dp, -1, sizeof(dp));
     cin >> n >> m >> c;
-    for (int i=1; i<=c; ++i) {
+    for (int i = 1; i <= c; ++i) {
         cin >> y >> x;
-        a[y][x]=i;
+        --y; --x;
+        a[y][x] = i;
     }
-    for (int i=0; i<=c; ++i) {
-        cout << go(1,1,i,0) << ' ';
+
+    if (a[0][0]) {
+        dp[0][0][1][a[0][0]] = 1;
+    }
+    else {
+        dp[0][0][0][0] = 1;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (j < m-1 && a[i][j + 1]) {
+                for (int k = 1; k <= c; ++k) { // cnt
+                    for (int p = 0; p < a[i][j + 1]; ++p) { // prev
+                        dp[i][j + 1][k][a[i][j + 1]] += dp[i][j][k - 1][p];
+                        dp[i][j + 1][k][a[i][j + 1]] %= mod;
+                    }
+                }
+            }
+            else {
+                for (int k = 0; k <= c; ++k) { // cnt
+                    for (int p = 0; p <= c; ++p) { // prev
+                        dp[i][j + 1][k][p] += dp[i][j][k][p];
+                        dp[i][j + 1][k][p] %= mod;
+                    }
+                }
+            }
+
+            //for (int k = 0; k <= c; ++k) {
+            //    for (int i = 0; i < n; ++i) {
+            //        for (int j = 0; j < m; ++j) {
+            //            int sum = 0;
+            //            for (int p = 0; p <= c; ++p) {
+            //                sum += dp[i][j][k][p];
+            //            }
+            //            cout << sum << " ";
+            //        }
+            //        cout << "\n";
+            //    }
+            //    cout << "\n";
+            //}
+
+
+            if (i < n-1 && a[i + 1][j]) {
+                for (int k = 1; k <= c; ++k) { // cnt
+                    for (int p = 0; p < a[i + 1][j]; ++p) { // prev
+                        dp[i + 1][j][k][a[i + 1][j]] += dp[i][j][k - 1][p];
+                        dp[i + 1][j][k][a[i + 1][j]] %= mod;
+                    }
+                }
+            }
+            else {
+                for (int k = 0; k <= c; ++k) { // cnt
+                    for (int p = 0; p <= c; ++p) { // prev
+                        dp[i + 1][j][k][p] += dp[i][j][k][p];
+                        dp[i + 1][j][k][p] %= mod;
+                    }
+                }
+            }
+
+
+
+            //for (int k = 0; k <= c; ++k) {
+            //    for (int i = 0; i < n; ++i) {
+            //        for (int j = 0; j < m; ++j) {
+            //            int sum = 0;
+            //            for (int p = 0; p <= c; ++p) {
+            //                sum += dp[i][j][k][p];
+            //            }
+            //            cout << sum << " ";
+            //        }
+            //        cout << "\n";
+            //    }
+            //    cout << "\n";
+            //}
+
+        }
+    }
+
+    for (int i = 0; i <= c; ++i) {
+        int sum = 0;
+        for (int j = 0; j <= c; ++j) {
+            sum += dp[n - 1][m - 1][i][j];
+            sum %= mod;
+        }
+        cout << sum << " ";
     }
 
     return 0;
