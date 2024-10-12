e4478de5bd66b36de82f65b9472a890a4afa0c43
Chaeyoon Lee <engrrachel318@gmail.com>
2024-10-13 06:49:18 +0900

    [Silver I] Title: 용돈 관리, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.cc
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.cc
@@ -1,41 +1,46 @@
-#include <bits/stdc++.h>
-using namespace std;  
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
-const int INF=1e9;
-int n, m, a[100001], mx, lo, hi, ret=INF;
+int n, m, a[100001], ret, mx;
 
-int check(int mid) {
-    int bal=0, cnt=0;
-    for (int i=0; i<n; ++i) {
-        if (bal<a[i]) bal=mid, ++cnt;
-        bal-=a[i];
+bool check(int money) {
+    //printf("money : %d\n", money);
+    int cur = 0, cnt = 0;
+    for (int i = 0; i < n; ++i) {
+        if (cur < a[i]) {
+            cur = money;
+            ++cnt;
+        }
+        cur -= a[i];
+
+        //printf("cur : %d, cnt : %d\n", cur, cnt);
     }
-    return cnt;
+    return cnt <= m;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-    scanf("%d %d", &n, &m);
-    for (int i=0; i<n; ++i) {
-        scanf("%d", a+i);
-        mx=max(mx,a[i]);
-        hi+=a[i];
+    cin >> n >> m;
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        mx = max(mx, a[i]);
     }
 
-    lo=1;
-    while(lo<=hi) {
-        int mid = (lo+hi)/2;
-        if (mid<mx||check(mid)>m) {
-            lo=mid+1;
-        } else {
-            ret=min(ret,mid);
-            hi=mid-1;
-        } 
+    int l = mx, r = int(1e9);
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+        if (check(mid)) {
+            ret = mid;
+            r = mid - 1;
+        }
+        else l = mid + 1;
     }
-    printf("%d\n", ret);
+
+    cout << ret << "\n";
 
     return 0;
 }
