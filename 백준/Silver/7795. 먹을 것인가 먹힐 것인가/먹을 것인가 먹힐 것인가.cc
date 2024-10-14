df70014394b49b498648a23511003b2f69ddd173
Chaeyoon Lee <engrrachel318@gmail.com>
2024-10-14 14:19:04 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 28 ms, Memory: 2224 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
@@ -1,30 +1,37 @@
-#include <bits/stdc++.h>
-using namespace std;  
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-int a[20000], b[20000], n, m, T, ret;
+vector<int> a, b;
+int t, n, m, ret;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-    scanf("%d", &T);
-    while(T--) {
-        ret=0;
-        scanf("%d %d", &n, &m);
-        for (int i=0; i<n; ++i) {
-            scanf("%d", a+i);
-        }
-        for (int i=0; i<m; ++i) {
-            scanf("%d", b+i);
-        }
-        sort(a, a+n);
-        sort(b, b+m);
+    cin >> t;
+    while (t--) {
+        cin >> n >> m;
+        a.resize(n);
+        b.resize(m);
 
-        for (int i=0; i<m; ++i) {
-            ret+=a+n-upper_bound(a,a+n,b[i]);
+        for (int i = 0; i < n; ++i)
+            cin >> a[i];
+        for (int i = 0; i < m; ++i)
+            cin >> b[i];
+
+        ret = 0;
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+
+        for (int bval : b) {
+            auto it = upper_bound(a.begin(), a.end(), bval);
+            ret += a.size() - (it - a.begin());
         }
-        printf("%d\n",ret);
+
+        cout << ret << "\n";
     }
 
     return 0;
