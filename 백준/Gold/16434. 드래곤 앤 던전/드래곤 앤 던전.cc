17e5efe84fe48d4e673de991dec5fc99c7bd1b79
Chaeyoon Lee <engrrachel318@gmail.com>
2024-10-16 00:02:53 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 96 ms, Memory: 4916 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
@@ -1,45 +1,55 @@
-#include <bits/stdc++.h>
-using namespace std;  
+#include <iostream>
+#include <limits>
+using namespace std;
 
 typedef long long ll;
-const ll INF=1e18;
-ll hCur, lo, hi, ret=INF, atk, n, t[123457], atkMon[123457], hMon[123457];
+struct room { ll t, a, h; } rm[123457];
+int n;
+ll atk_i, ret;
 
-bool check(ll mid) {
-    hCur=mid;
-    ll curAtk=atk;
-    for (int i=0; i<n; ++i) {
-        if (t[i]==1) {
-            ll pr = (hCur+atkMon[i]-1)/atkMon[i];
-            ll pm = (hMon[i]+curAtk-1)/curAtk;
-            if (pm<=pr) hCur -= atkMon[i]*(pm-1);
-            else return false;
-        } else {
-            curAtk+=atkMon[i];
-            hCur=min(mid,hCur+hMon[i]);
+bool check(ll hp_max) {
+    ll hp = hp_max;
+    ll atk = atk_i;
+    for (int i = 0; i < n; ++i) {
+        if (rm[i].t == 1) {
+            const ll& mon_atk = rm[i].a;
+            ll mon_hp = rm[i].h;
+
+            ll num_round = mon_hp / atk;
+            if (mon_hp % atk) ++num_round;
+            if (hp - mon_atk * (num_round - 1) <= 0) return false;
+            else hp -= mon_atk * (num_round - 1);
+        }
+        else {
+            const ll& atk_plus = rm[i].a;
+            const ll& hp_plus = rm[i].h;
+
+            hp = min(hp + hp_plus, hp_max);
+            atk += atk_plus;
         }
     }
     return true;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> n >> atk_i;
+    for (int i = 0; i < n; ++i)
+        cin >> rm[i].t >> rm[i].a >> rm[i].h;
 
-    scanf("%lld %lld", &n, &atk);
-    for (int i=0; i<n; ++i) {
-        scanf("%lld %lld %lld", t+i, atkMon+i, hMon+i);
-    }
-
-    lo=1, hi=INF;
-    while(lo<=hi) {
-        ll mid=(lo+hi)/2;
+    ll lo = 1, hi = numeric_limits<ll>::max();
+    while (lo <= hi) {
+        ll mid = lo + (hi - lo) / 2;
         if (check(mid)) {
-            ret=min(ret,mid);
-            hi=mid-1;
-        } else lo=mid+1;
+            ret = mid;
+            hi = mid - 1;
+        }
+        else lo = mid + 1;
     }
-    printf("%lld\n", ret);
+
+    cout << ret << "\n";
+
     return 0;
 }
