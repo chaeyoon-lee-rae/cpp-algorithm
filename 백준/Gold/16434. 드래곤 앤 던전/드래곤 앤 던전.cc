33f6ed35674a63faaffd6cf284d5e0aac4288c0e
Chaeyoon Lee <engrrachel318@gmail.com>
2024-10-16 10:07:10 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
@@ -1,55 +1,34 @@
 #include <iostream>
-#include <limits>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 
 typedef long long ll;
-struct room { ll t, a, h; } rm[123457];
-int n;
-ll atk_i, ret;
-
-bool check(ll hp_max) {
-    ll hp = hp_max;
-    ll atk = atk_i;
-    for (int i = 0; i < n; ++i) {
-        if (rm[i].t == 1) {
-            const ll& mon_atk = rm[i].a;
-            ll mon_hp = rm[i].h;
-
-            ll num_round = mon_hp / atk;
-            if (mon_hp % atk) ++num_round;
-            if (hp - mon_atk * (num_round - 1) <= 0) return false;
-            else hp -= mon_atk * (num_round - 1);
-        }
-        else {
-            const ll& atk_plus = rm[i].a;
-            const ll& hp_plus = rm[i].h;
-
-            hp = min(hp + hp_plus, hp_max);
-            atk += atk_plus;
-        }
-    }
-    return true;
-}
+ll n, atk, t, a, h, h_plus, mx;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    cin >> n >> atk_i;
-    for (int i = 0; i < n; ++i)
-        cin >> rm[i].t >> rm[i].a >> rm[i].h;
 
-    ll lo = 1, hi = numeric_limits<ll>::max();
-    while (lo <= hi) {
-        ll mid = lo + (hi - lo) / 2;
-        if (check(mid)) {
-            ret = mid;
-            hi = mid - 1;
+    cin >> n >> atk;
+    for (int i = 0; i < n; ++i) {
+        cin >> t >> a >> h;
+        if (t == 1) {
+            ll damage = a * ll(ceil(double(h) / atk) - 1);
+            if (h_plus < damage) {
+                mx += damage - h_plus;
+                h_plus = 0;
+            }
+            else 
+                h_plus -= damage;
         }
-        else lo = mid + 1;
+        else {
+            atk += a;
+            h_plus = min(h_plus + h, mx);
+        }
     }
 
-    cout << ret << "\n";
+    cout << mx + 1 << "\n";
 
     return 0;
 }
