59ef405059194f528fe9d9c0a4ccf6c989f64211
Chaeyoon Lee <engrrachel318@gmail.com>
2024-10-22 20:43:51 +0900

    [Gold II] Title: 색종이 붙이기, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.cc
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.cc
@@ -1,65 +1,60 @@
-#include <bits/stdc++.h>
-using namespace std;  
+#include <iostream>
+using namespace std;
 
-const int INF=1e9;
-int a[11][11], n, ret=INF, cp[11];
+const int n = 10, INF = 1e9;
+int ret = INF, p[6];
+bool a[11][11];
 
-void draw(int y, int x, int len, int val) {
-    for (int i=y; i<y+len; ++i) {
-        for (int j=x; j<x+len; ++j) {
-            a[i][j]=val;
-        }
-    }
-    return;
-}
-
-bool check(int y, int x, int len) {
-    if (y+len>10||x+len>10) return false;
-    for (int i=y; i<y+len; ++i) {
-        for (int j=x; j<x+len; ++j) {
-            if (a[i][j]==0) return false;
-        }
-    }
+bool checkSquare(int y, int x, int k) {
+    if (y + k > n || x + k > n) return false;
+    for (int i = y; i < y + k; ++i)
+        for (int j = x; j < x + k; ++j)
+            if (a[i][j] == 0) return false;
     return true;
 }
 
-void dfs(int y, int x, int cnt) {
-    if (cnt>=ret) return;
-    if (x==10) {
-        dfs(y+1, 0, cnt); return;
+void fillSquare(int y, int x, int k, int val) {
+    for (int i = y; i < y + k; ++i)
+        for (int j = x; j < x + k; ++j)
+            a[i][j] = val;
+}
+
+void go(int i, int j, int cnt) {
+    if (cnt >= ret) return;
+    if (j == n) {
+        go(i + 1, 0, cnt);
+        return;
     }
-    if (y==10) {
-        ret=min(ret,cnt); return;
+    if (i == n) {
+        ret = min(ret, cnt);
+        return;
     }
-    if (a[y][x]==0) {
-        dfs(y,x+1,cnt); return;
+    if (a[i][j] == 0) {
+        go(i, j + 1, cnt);
+        return;
     }
-    for (int i=1; i<=5; ++i) {
-        if (cp[i]==5) continue;
-        if (check(y,x,i)) {
-            ++cp[i];
-            draw(y,x,i,0);
-            dfs(y,x+i,cnt+1);
-            --cp[i];
-            draw(y,x,i,1);
+
+    for (int k = 1; k <= 5; ++k) {
+        if (checkSquare(i, j, k) && p[k] < 5) {
+            fillSquare(i, j, k, 0);
+            ++p[k];
+            go(i, j + 1, cnt + 1);
+            fillSquare(i, j, k, 1);
+            --p[k];
         }
     }
-    return;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    for (int i=0; i<10; ++i) {
-        for (int j=0; j<10; ++j) {
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
             cin >> a[i][j];
-        }
-    }
 
-    dfs(0,0,0);
-    cout << (ret==INF ? -1 : ret) << '\n';
+    go(0, 0, 0);
+    cout << (ret == INF ? -1 : ret) << "\n";
 
     return 0;
 }
