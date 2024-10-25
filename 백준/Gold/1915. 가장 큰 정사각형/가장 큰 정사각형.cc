4835fe1a43e4a9b93a104cccfe6c17bd6b1724b5
Chaeyoon Lee <engrrachel318@gmail.com>
2024-10-25 21:31:18 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 16 ms, Memory: 6912 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001][1001], n, m, ret;
+bool a[1001][1001];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+    char ch;
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= m; ++j) {
+            cin >> ch;
+            a[i][j] = ch - '0';
+        }
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            if (a[i][j])
+                dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+            ret = max(ret, dp[i][j]);
+        }
+    }
+
+    cout << ret * ret << "\n";
+
+    return 0;
+}
