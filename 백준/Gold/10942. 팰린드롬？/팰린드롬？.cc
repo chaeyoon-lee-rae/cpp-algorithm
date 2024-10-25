c088b81d73661bfe64c5e050d6456e5be631466d
Chaeyoon Lee <engrrachel318@gmail.com>
2024-10-25 22:02:50 +0900

    [Gold IV] Title: 팰린드롬?, Time: 216 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -1,31 +1,36 @@
-#include <bits/stdc++.h>
-using namespace std;  
+#include <iostream>
+using namespace std;
 
-bool dp[2001][2001];
 int n, a[2001], m, s, e;
+bool dp[2001][2001];
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
     cin >> n;
-    for (int i=1; i<=n; ++i) cin >> a[i];
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        dp[i][i] = 1;
+    }
 
-    for (int i=1; i<=n; ++i) dp[i][i]=1;
-
-    for (int i=1; i<=n-1; ++i)
-        if (a[i]==a[i+1]) dp[i][i+1]=1;
-
-    for (int len=2; len<n; ++len)
-        for (int i=1; i<=n-len; ++i)
-            if (a[i]==a[i+len]&&dp[i+1][i+len-1]) dp[i][i+len]=1;
+    for (int i = 0; i < n - 1; ++i) {
+        if (a[i] == a[i + 1])
+            dp[i][i + 1] = 1;
+    }
+    for (int len = 2; len < n; ++len)
+        for (int i = 0; i + len < n; ++i)
+            if (a[i] == a[i + len])
+                dp[i][i + len] = dp[i + 1][i + len - 1];
 
     cin >> m;
-    for (int i=0; i<m; ++i) {
+    while (m--) {
         cin >> s >> e;
-        cout << dp[s][e] << '\n';
+        --s, --e;
+        cout << dp[s][e] << "\n";
     }
+
 
     return 0;
 }
